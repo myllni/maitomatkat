3cfc29c7a143cf29617ea9a9df50d533e4cad763
myllni <niilomyllynen@gmail.com>
2024-10-03 11:56:11 +0300

    Kaavion sahausta

--- kaavio.js
+++ kaavio.js
@@ -1,10 +1,8 @@
 
-<!--Avaa kuvan-->
 function openModal() {
   document.getElementById("kaavio").style.display = "block";
 }
 
-<!-- sulkee kuvan-->
 function closeModal() {
   document.getElementById("kaavio").style.display = "none";
 }
